a500cd372359c7253208bda15eaa5b635eff1ffd
Liang, Yangxing <yangxing_liang@cathaypacific.com>
2017-12-08 15:42:59 +0800

    update model

--- model/model.go
+++ model/model.go
@@ -1,11 +1,21 @@
 package model
 
+import (
+	"time"
+)
+
 type File struct {
 	Id      string  `json:"id"`
 	Name    string  `json:"name"`
 	Type    string  `json:"pretty_type"`
 	Size    float64 `json:"size"`
 	Created int64   `json:"created"`
+}
+
+func (f File) CreatedDateWithoutTime() time.Time {
+	date := time.Unix(f.Created, 0)
+	date = time.Date(date.Year(), date.Month(), date.Day(), 0, 0, 0, 0, time.Local)
+	return date
 }
 
 type Paging struct {
@@ -18,4 +28,10 @@ type ListResponse struct {
 	Error    string `json:"error,omitempty"`
 	Files    []File `json:"files"`
 	PageInfo Paging `json:"paging"`
+}
+
+type AuthResponse struct {
+	Ok    bool   `json:"ok"`
+	User  string `json:"user_id"`
+	Error string `json:"error"`
 }
